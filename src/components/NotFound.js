fdceab0fd0e58b0a7f9f8db604b9c2c717460de8
FoxyDaDev <150698022+FoxyDaDev@users.noreply.github.com>
2023-11-17 23:16:37 +0330

    Update NotFound.js
    Removed bad words

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function NotFound() {
     return (
-        <h1>YOU DUMBFUCK GIVE ME A PROPER LINK</h1>
+        <h1>YOU good human GIVE ME A PROPER LINK</h1>
     );
 }
