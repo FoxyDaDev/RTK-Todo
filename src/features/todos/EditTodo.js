b4b09dc6da2be20de5ed06c775d79e9ec5fc3f1a
FoxyDaDev <150698022+FoxyDaDev@users.noreply.github.com>
2023-11-20 19:01:02 +0330

    Update EditTodo.js
    Made it simpler, added defaultValue and stuff

--- src/features/todos/EditTodo.js
+++ src/features/todos/EditTodo.js
@@ -1,111 +1,104 @@
-import { useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectTodoById, editTodo, removeTodo } from "./todoSlice";
 import { useParams, useNavigate } from "react-router-dom";
-import { selectAllUsers } from "../users/authorSlice";
 
 const EditTodo = () => {
-    const { todoId } = useParams();
-    const navigate = useNavigate();
+  const { todoId } = useParams();
+  const navigate = useNavigate();
 
-    const todo = useSelector((state) => selectTodoById(state, todoId))
-    const users = useSelector(selectAllUsers)
+  const todo = useSelector((state) => selectTodoById(state, todoId));
+  const users = useSelector((state) => state.users);
 
-    const [title, setTitle] = useState(todo?.title)
-    const [content, setContent] = useState(todo?.content)
-    const [userId, setUserId] = useState(todo?.userId)
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
-
-    if (!todo) {
-        return (
-            <section>
-                <h1>todo not found!</h1>
-            </section>
-        )
-    }
-
-    const onTitleChanged = (e) => setTitle(e.target.value)
-    const onContentChanged = (e) => setContent(e.target.value)
-    const onAuthorChanged = (e) => setUserId(Number(e.target.value))
-    
-    const canSave = [title, content, userId].every(Boolean)
-
-    const onSaveTodo = () => {
-        if (canSave) {
-            try {
-                dispatch(editTodo({ id: todo.id, title, content, userId: Number(userId) }))
-
-                setTitle('')
-                setContent('')
-                setUserId('')
-                navigate(`/todo/${todoId}`)
-            } catch (err) {
-                console.log(`edit error found ${err}`)
-            }
-        }
-    }
-
-    const usersOptions = users.map(user => (
-        <option
-            key={user.id}
-            value={user.id}
-        >{user.name}</option>
-    ))
-
-    const onDeletePost = () => {    
-        try {
-            dispatch(removeTodo({ id: todo.id }));
-
-            setTitle('');
-            setContent('');
-            setUserId('');
-            navigate('/');
-        } catch (err) {
-            console.log(`remove error found ${err}`)
-        }
-    }
-
+  if (!todo) {
     return (
-        <section>
-            <h2>Edit Todo:</h2>
-            <form>
-                <label htmlFor="todoTitle">Todo Title:</label>
-                <input
-                    type="text"
-                    id="todoTitle"
-                    name="todoTitle"
-                    value={title}
-                    onChange={onTitleChanged}
-                />
-                <label htmlFor="todoAuthor">Author:</label>
-                <select id="todoAuthor" value={userId} onChange={onAuthorChanged}>
-                    <option value=""></option>
-                    {usersOptions}
-                </select>
-                <label htmlFor="todoContent">Content:</label>
-                <textarea
-                    id="todoContent"
-                    name="todoContent"
-                    value={content}
-                    onChange={onContentChanged}
-                />
-                <button
-                    type="button"
-                    onClick={onSaveTodo}
-                    disabled={!canSave}
-                >
-                    Save Todo
-                </button>
-                <button className="deleteButton"
-                    type="button"
-                    onClick={onDeletePost}
-                >
-                    Delete Todo
-                </button>
-            </form>
-        </section>
-    )
-}
+      <section>
+        <h1>Todo not found!</h1>
+      </section>
+    );
+  }
+
+  const onSaveTodo = (e) => {
+    e.preventDefault();
+
+    const { title, content, userId } = e.target.elements;
+
+    if (title.value && content.value) {
+      try {
+        dispatch(editTodo({
+          id: todo.id,
+          title: title.value,
+          content: content.value,
+          userId: Number(userId.value)
+        }));
+
+        title.value = "";
+        content.value = "";
+
+        navigate(`/todo/${todoId}`);
+      } catch (err) {
+        console.log(`Edit error found: ${err}`);
+      }
+    }
+  };
+
+  const onDeletePost = () => {
+    try {
+      dispatch(removeTodo({ id: todo.id }));
+
+      navigate('/');
+    } catch (err) {
+      console.log(`Remove error found: ${err}`);
+    }
+  };
+
+  const usersOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
+
+  return (
+    <section>
+      <h2>Edit Todo:</h2>
+      <form onSubmit={onSaveTodo}>
+        <label htmlFor="todoTitle">Todo Title:</label>
+        <input
+          type="text"
+          id="todoTitle"
+          name="title"
+          defaultValue={todo.title}
+        />
+        <label htmlFor="todoAuthor">Author:</label>
+        <select
+          id="todoAuthor"
+          name="userId"
+          defaultValue={todo.userId}
+        >
+          <option value=""></option>
+          {usersOptions}
+        </select>
+        <label htmlFor="todoContent">Content:</label>
+        <textarea
+          id="todoContent"
+          name="content"
+          defaultValue={todo.content}
+        />
+        <button type="submit">
+          Save Todo
+        </button>
+        <button
+          className="deleteButton"
+          type="button"
+          onClick={onDeletePost}
+        >
+          Delete Todo
+        </button>
+      </form>
+    </section>
+  );
+};
 
 export default EditTodo;
